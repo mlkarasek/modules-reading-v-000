5e8e00b0a72efb6e3a28cbd28f3f556a83967c98
Monica  Karasek <mlkarasek@gmail.com>
2017-10-28 02:34:26 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,12 +5,11 @@ require_relative './fancy_dance.rb'
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
-end
+
 
   attr_accessor :name
 
   def initialize(name)
     @name = name
   end
-
-
+end 
