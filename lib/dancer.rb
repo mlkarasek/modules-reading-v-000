d5e19067b05ad615209804421ee144bc362da62f
Monica  Karasek <mlkarasek@gmail.com>
2017-10-28 02:25:26 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,11 +2,11 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  include Dance 
+  include Dance
 
-  attr_accessor :name 
+  attr_accessor :name
 
   def initialize(name)
-    @name = name 
+    @name = name
   end 
-end 
+end
