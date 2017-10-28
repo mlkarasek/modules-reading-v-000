1e0c48b33fafb7259c3037f46c9b765214483514
Monica  Karasek <mlkarasek@gmail.com>
2017-10-28 02:23:44 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,11 +3,11 @@ require_relative './class_methods_module.rb'
 
 class Kid
 
-  include Dance 
+  include Dance
 
   attr_accessor :name
 
   def initialize(name)
-    @name = name 
+    @name = name
   end 
 end
