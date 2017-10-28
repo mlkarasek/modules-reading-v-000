cb68baa1746ebfc2282032920f2e47774f1855a7
Monica  Karasek <mlkarasek@gmail.com>
2017-10-28 02:32:11 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,8 +3,9 @@ require_relative './class_methods_module.rb'
 
 class Kid
 
-  include Dance
-  extend MetaDancing
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end 
 
   attr_accessor :name
 
